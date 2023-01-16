ed18fea54e49e56a6584c0291b27e0627aa277dc
satish <satishsuryawanshi680@gmail.com>
2023-01-16 23:35:14 +0530

    try8

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello $(hostname)"));
-const getIP = async () => ec2.describeInstances(params, (err, data) => {
-  if (err) return err
-  return data.Reservations[0].Instances[0].PublicIpAddress
-}).promise()
-
-const ip = await getIP()
-console.log(ip)
-
+app.get("/", (req, res) => res.send("Hello Youtube"));
 app.get("/health", (req, res) => {
   res.status(200);
   res.send("healthy");
