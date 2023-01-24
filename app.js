48698f1576ff7c4c827e2d1067d2f68af2db18a1
satish <satishsuryawanshi680@gmail.com>
2023-01-24 23:38:45 +0530

    try10

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Bala"));
+app.get("/", (req, res) => res.send("Hello "));
 app.get("/health", (req, res) => {
   res.status(200);
   res.send("healthy");
